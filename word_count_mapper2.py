a823be092cfcd2aa12e864463f4aa336a790bd34
Nguyễn Vũ Hải <nvhai.it.tn@gmail.com>
2023-03-26 21:37:52 +0700

    push new work

---
+++ word_count_mapper2.py
@@ -0,0 +1,13 @@
+
+import sys 
+
+
+for line in sys.stdin:
+
+    # remove leading and trailing whitespace 
+    line = line.strip() 
+    # split the line into words 
+    words = line.split() 
+    # increase counters 
+    for word in words: 
+        print('%s\t%s' % (word, 1))
